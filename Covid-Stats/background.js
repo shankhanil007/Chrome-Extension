3a7aaf244bb2271b9f413a0b24a39eb2d4af9cc9
shankhanil007 <shankhanilborthakur@gmail.com>
2021-07-02 11:42:24 +0530

    Foreground JS

--- Covid-Stats/background.js
+++ Covid-Stats/background.js
@@ -1,71 +1,13 @@
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   console.log(tabId);
-//   console.log(changeInfo);
-//   console.log(tab);
-// });
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({
-    name: "Jack",
-  });
-});
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
     chrome.scripting
-      .insertCSS({
+      .executeScript({
         target: { tabId: tabId },
-        files: ["./foreground_styles.css"],
+        files: ["./foreground.js"],
       })
       .then(() => {
-        console.log("INJECTED THE FOREGROUND STYLES.");
-
-        chrome.scripting
-          .executeScript({
-            target: { tabId: tabId },
-            files: ["./foreground.js"],
-          })
-          .then(() => {
-            console.log("INJECTED THE FOREGROUND SCRIPT.");
-          });
+        console.log("INJECTED THE FOREGROUND SCRIPT.");
       })
       .catch((err) => console.log(err));
-  }
-});
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "get_name") {
-    chrome.storage.local.get("name", (data) => {
-      if (chrome.runtime.lastError) {
-        sendResponse({
-          message: "fail",
-        });
-
-        return;
-      }
-
-      sendResponse({
-        message: "success",
-        payload: data.name,
-      });
-    });
-
-    return true;
-  } else if (request.message === "change_name") {
-    chrome.storage.local.set(
-      {
-        name: request.payload,
-      },
-      () => {
-        if (chrome.runtime.lastError) {
-          sendResponse({ message: "fail" });
-          return;
-        }
-
-        sendResponse({ message: "success" });
-      }
-    );
-
-    return true;
   }
 });
