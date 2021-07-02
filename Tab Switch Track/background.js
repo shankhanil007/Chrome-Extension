103a1dd90cd6fbdda9c5e1104723a933baf1641f
shankhanil007 <shankhanilborthakur@gmail.com>
2021-07-02 16:58:12 +0530

    Tab Switch Count Detected

--- Tab Switch Track/background.js
+++ Tab Switch Track/background.js
@@ -1,19 +1,23 @@
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
-//     chrome.scripting
-//       .executeScript({
-//         target: { tabId: tabId },
-//         files: ["./foreground.js"],
-//       })
-//       .then(() => {
-//         console.log("INJECTED THE FOREGROUND SCRIPT.");
-//       })
-//       .catch((err) => console.log(err));
-//   }
+let count = 0;
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.local.clear();
+//   chrome.storage.local.set({ count: 0 });
 // });
 
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   console.log(tabId);
-//   console.log(changeInfo);
-//   console.log(tab);
-// });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+    count = count + 1;
+    chrome.storage.local.set({ count: count }, function () {
+      chrome.storage.local.get(null, function (data) {
+        console.log(data.count);
+      });
+    });
+  }
+
+  //   console.log(localStorage.getItem("count"));
+  //   chrome.storage.local.set({
+  //     count: count + 1,
+  //   });
+  //   console.log(chrome.storage.local.get("count"));
+});
