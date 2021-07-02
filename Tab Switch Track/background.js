9f615f5cecd1e78445c864dae15b94273bdc0fcd
shankhanil007 <shankhanilborthakur@gmail.com>
2021-07-02 13:13:27 +0530

    Project Setup

---
+++ Tab Switch Track/background.js
@@ -0,0 +1,19 @@
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+//     chrome.scripting
+//       .executeScript({
+//         target: { tabId: tabId },
+//         files: ["./foreground.js"],
+//       })
+//       .then(() => {
+//         console.log("INJECTED THE FOREGROUND SCRIPT.");
+//       })
+//       .catch((err) => console.log(err));
+//   }
+// });
+
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   console.log(tabId);
+//   console.log(changeInfo);
+//   console.log(tab);
+// });
